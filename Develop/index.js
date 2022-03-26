3ec3dfd7fb63750e848a16ba7a5255b78694a554
JacobReeder <jacob.reeder1@gmail.com>
2022-03-26 10:14:51 -0700

    More pseudocode was added. nothing functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,9 @@
 //ADDED FROM MODULE PROJECT(keep in this order) //
-const inquirer = require('inquirer'); // Add descrip
-const fs = require('fs'); // Add descrip
-const pageRead = require(''); // Add descrip // should this be defined as pageRead instead?
-
+//const inquirer = require('inquirer'); // Add descrip
+const fs = require('fs'); // The require statement is a built-in function that's globally available in Node.js. It allows the app.js file to access the fs module's functions through the fs assignment.
+const pageRead = require(''); // use this to define file path
+//const [//questions input goes here?] =
+//define 'fileName' and 'data'
 
 
 
@@ -14,6 +15,46 @@ const pageRead = require(''); // Add descrip // should this be defined as pageRe
   
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//NOTES
+//process.argv.slice(2, process.argv.length - 1);? It turns out .slice() returns everything from the first 
+////zero-based index we provide as the first argument up to but not including the zero-based index 
+//we provide as the second argument (9.1.5)
+
+//when we use the const keyword instead of var to create a variable,
+// we're locking down what that variable will be. Once we declare it, we can never reassign it. 
+
+/////////////ARRAY EXAMPLE AND DESCRIP/////////////////////////////////////////////////////////////////
+
+/*const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+  name: 'Lernantino',
+  age: 99
+};
+
+personObj.age = 100;
+personObj.occupation = 'Developer';
+If we ran the preceding code, we wouldn't get an error, and the .push() method would work as expected. We could also add, edit, or remove properties from the personObj without an error. So what's the difference between this example and the previous one?
+
+In the example with message and sum, we attempted to directly assign a brand-new value to the variables. In the second example, we didn't reassign the values of animalArray and personObj but rather edited the content inside those values.
+
+The JavaScript engine sees those const variable values and just sees an array and an object. It has no idea what's going on between those square or curly brackets.
+
+*/
+
+//* Using function expression syntax
+/*const addNums = function(numOne, numTwo) {
+  return numOne + numTwo;
+};
+
+// Using new arrow function syntax
+const addNums = (numOne, numTwo) => {
+  return numOne + numTwo;
+};*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // TODO: Create an array of questions for user input
 //const questions = [
@@ -24,7 +65,8 @@ const pageRead = require(''); // Add descrip // should this be defined as pageRe
    //ALSO
    //Create a new README?
 
-   const questions = () => {
+   /*const questions = () => {
+     
     return inquirer.prompt([
       {
           type: 'input',
@@ -151,7 +193,7 @@ const pageRead = require(''); // Add descrip // should this be defined as pageRe
         } else {
           return portfolioData;
         }
-  });
+  });*/
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -161,15 +203,15 @@ const pageRead = require(''); // Add descrip // should this be defined as pageRe
 
 
 // TODO: Create a function to write README file ///Reference earlier in module 9 - lesson 2 and 3 -
-function writeToFile(fileName, data) {
-
-  fs.writeFile('./README.md', pageRead, err => {
+//*function writeToFile(/*fileName,*/ data) { //equivalent of generatePage in 9.2.4
+      
+  //fs.writeToFile('./README.md', writeToFile, err => {
     // If there is any error in writing to the file, return
     //   if (err) throw new Error(err);
-    if (err) {
-      console.error(err)
-      return
-    }
+    //if (err) {
+     // console.error(err)
+    ///  return
+    /*}
   
     // Log this message if the file was written to successfully
     console.log('wrote to file successfully')
@@ -177,9 +219,11 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+//*function init() {}
 
 
 // Function call to initialize app
-init();
-const pageRead = writeToFile();
+//*init();
+//*const pageRead = writeToFile();*/
+
+
